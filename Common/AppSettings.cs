ee1e9aea27168415a5bcc2f5e8ec0d6a67d431c7
Raul202 <55659437+RiceaRaul@users.noreply.github.com>
2022-12-30 15:42:42 +0200

    Update AppSettings.cs

--- Common/AppSettings.cs
+++ Common/AppSettings.cs
@@ -1,9 +1,9 @@
-﻿namespace Common
+namespace Common;
+
+public class AppSettings
 {
-    public class AppSettings
-    {
-        public string Token { get; set; } = default!;
-        public char Prefix { get; set; } = default!;
-        public UInt64 Guild { get; set; } = default!;
-    }
+    public string Token { get; set; } = default!;
+    public char Prefix { get; set; } = default!;
+    public UInt64 Guild { get; set; } = default!;
 }
+
